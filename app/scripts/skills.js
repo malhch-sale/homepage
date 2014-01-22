95aa713a2d5a19be36f93af46a90918bb99e2573
Ma Lian <malhch@gmail.com>
2014-01-22 09:27:11 +0800

    2014-01-22 09:26 Update: d3 skills

--- app/scripts/skills.js
+++ app/scripts/skills.js
@@ -1,8 +1,9 @@
 require(['d3'], function(d3) {
-    var containerHeight = 600,
+    var containerHeight = 700,
         container = d3.select(d3.select('#skills').node().parentNode).append('div')
             .style('height', containerHeight + 'px')
-            .style('overflow', 'hidden'),
+            .style('overflow', 'hidden')
+            .style('border', '1px solid silver'),
         containerWidth = container.node().getBoundingClientRect().width,
         canvasWidth = containerWidth + 200,
         canvasHeight = containerHeight + 300,
@@ -27,68 +28,115 @@ require(['d3'], function(d3) {
                 name: 'Skill',
                 displayName: '',
                 links: [],
-                categories: {
-                    Skill: 1
-                },
+                categories: {},
                 fixed: true,
                 x: width / 2,
                 y: height / 2,
                 radius: 40,
-                charge: -800
+                charge: -300
             }];
         parseNodesAndLinks(res, nodes, links);
         prepareNodesAndLinks(nodes, links);
 
-        force = d3.layout.force()
-            .size([width, height])
-            .nodes(nodes).links(links)
-        /*.gravity(0)
-            .friction(0.9)*/
-        .linkStrength(10)
-            .linkDistance(100)
-            .charge(function(node) {
-                return -400;
+        /**
+         * 配置 force
+         */
+        var force = d3.layout.force().size([width, height]).gravity(0.05)
+            .nodes(nodesFilter(nodes)).links(linksFilter(links))
+            .linkStrength(function fn(link) {
+                return link.strength;
+            }).linkDistance(function fn(link) {
+                return link.distance;
+            }).charge(function fn(node) {
+                return node.charge;
+            });
+        drawNodesAndLinks(force);
+
+        /**
+         * 动画设计
+         */
+        force.start().on('tick', function(options) {
+            // 每次 tick 都要重新把所有节点四分化
+            var q = d3.geom.quadtree(force.nodes());
+            force.nodes().forEach(function(node) {
+                q.visit(collide(node, options.alpha));
             });
 
-        var glinks = svg.selectAll('.link').data(force.links())
-            .enter().append('line').attr('class', 'link');
-
-        var gnodes = svg.selectAll('.node').data(force.nodes())
-            .enter().append('g').attr('class', 'node');
-        gnodes.append('circle');
-        gnodes.append('text').text(function fn(node) {
-            return node.name;
-        }).attr('transform', function fn() {
-            return 'scale(0.5)';
-        }).attr('dx', function fn(node) {
-            return -d3.select(this).node().getBBox().width / 2;
-        }).attr('dy', function fn(node) {
-            return d3.select(this).node().getBBox().height / 2;
-        })
-
-        force.start()
-            .on('tick', function(options) {
-                svg.selectAll('.node').each(function fn(node) {
-                    var g = d3.select(this).attr("transform", translate);
-
-                    g.select('circle').attr('r', g.select('text').node().getBBox().width / 4 + 5);
-                });
-            })
-            .on('end', function() {
-                svg.selectAll('.link').each(function fn(link) {
-                    d3.select(this).attr({
-                        x1: link.source.x,
-                        y1: link.source.y,
-                        x2: link.target.x,
-                        y2: link.target.y
-                    });
-                });
+            // 避免挤出画面
+            var alpha = 0.1; //options.alpha;
+            force.nodes().forEach(function(node) {
+                var d = 50;
+                node.x < d && (node.x += (d - node.x) * alpha);
+                node.x > width - d && (node.x += (width - d - node.x) * alpha);
+                node.y < d && (node.y += (d - node.y) * alpha);
+                node.y > height - d && (node.y += (height - d - node.y) * alpha);
             });
 
-        function translate(node) {
-            return "translate(" + node.px + "," + node.py + ")";
-        }
+            // 画节点
+            svg.selectAll('.node').each(function fn(node) {
+                d3.select(this).attr("transform", "translate(" + node.x + "," + node.y + ")");
+
+                if (node.group === 'skill') {
+                    var dx = node.x - width / 2,
+                        dy = node.y - height / 2,
+                        a = Math.atan2(dx, dy),
+                        // here because the bug for can't selecting foreignObject
+                        f = this.childNodes[1];
+                    f.setAttribute('x', node.width / node.scale * (dx > 0 ? -1 : 0) + (dx > 0 ? -5 : 5))
+                    f.setAttribute('y', node.height / node.scale * (dy > 0 ? -1 : 0));
+                } else if (node.name !== 'Skill') {
+                    var link = node.links[0],
+                        dx = link.target.x - link.source.x,
+                        dy = link.target.y - link.source.y,
+                        f = this.childNodes[1];
+                    f.setAttribute('x', node.width / node.scale * (dx > 0 ? 0 : -1) + (dx > 0 ? 3 : -3))
+                    f.setAttribute('y', node.height / node.scale * (dy > 0 ? 0 : -1));
+                }
+            });
+
+            // 画连线
+            svg.selectAll('.link').each(function fn(link) {
+                d3.select(this).attr({
+                    x1: link.source.x,
+                    y1: link.source.y,
+                    x2: link.target.x,
+                    y2: link.target.y
+                });
+            });
+        });
+
+        $('.skills :checkbox').change(function(event) {
+            force.nodes(nodesFilter(nodes)).links(linksFilter(links));
+            drawNodesAndLinks(force);
+            force.start();
+        });
     });
+
+    function nodesFilter(nodes) {
+        var checked = [];
+        $('.skills :checkbox').each(function fn(index, el) {
+            el.checked && checked.push(el.name);
+        });
+        return nodes.filter(function(node) {
+            return checked.some(function fn(category) {
+                return node.categories && node.categories[category];
+            });
+        });
+    }
+
+    function linksFilter(links) {
+        var checked = [];
+        $('.skills :checkbox').each(function fn(index, el) {
+            el.checked && checked.push(el.name);
+        });
+        return links.filter(function(link) {
+            return checked.some(function fn(category) {
+                var s = link.source.categories,
+                    t = link.target.categories;
+                return s && s[category] && t && t[category];
+            });
+        });
+    }
 
     function parseNodesAndLinks(res, nodes, links) {
         var parent, group, name, title, link, pnode, cnode, names, path, category, map = {};
@@ -165,14 +213,14 @@ require(['d3'], function(d3) {
          */
         links.forEach(function fn(link) {
             if (link.source.name === 'Skill') {
-                link.strength = 0.5;
-                link.distance = 120;
+                link.strength = 1;
+                link.distance = 80;
             } else if (link.source.group === 'skill') {
                 link.strength = 1;
                 link.distance = 40;
             }
 
-            link.strength = link.strength || 0.5;
+            link.strength = link.strength || 2;
             link.distance = link.distance || 20;
             link.class = link.class || 'link';
         });
@@ -181,13 +229,82 @@ require(['d3'], function(d3) {
             node.title && node.class.push('real');
 
             if (node.group === 'skill') {
-                node.charge = -500;
+                node.charge = -1000;
                 node.class.push('skill');
             } else if (node.group === 'combine') {
                 node.class.push('combine');
             }
             node.class = node.class.join(' ');
-            node.charge = node.charge || -50;
+            node.charge = node.charge || -300;
         });
+    }
+
+    function drawNodesAndLinks(force) {
+        var gnodes = svg.selectAll('.node').data(force.nodes());
+
+        // 这两行没用，加上反而无法正常显示内容
+        //.attr('requiredExtensions', "http://www.w3.org/1999/xhtml")
+        //.append('xhtml:body').attr('xmlns', "http://www.w3.org/1999/xhtml")
+        var enter = gnodes.enter().append('g').attr('class', function(node) {
+            return node.class;
+        });
+        enter.append('circle').attr('r', function(node) {
+            return node.links.length + 1;
+        });
+        enter.append('foreignObject').append('xhtml:a').text(function fn(node) {
+            return typeof node.displayName === 'undefined' ? node.name : node.displayName;
+        });
+        gnodes.exit().remove();
+
+        gnodes.each(function(node) {
+            var w, h, rw, rh, scale = 0.75,
+                f = this.childNodes[1],
+                a = f.childNodes[0];
+            rw = (w = a.offsetWidth) * scale, rh = (h = a.offsetHeight) * scale;
+            f.setAttribute('width', w);
+            f.setAttribute('height', h);
+            f.setAttribute('x', 3);
+            f.setAttribute('y', 0);
+            f.setAttribute('transform', 'scale(' + scale + ')');
+
+            node.width = rw, node.height = rh, node.scale = scale;
+            //g.append('ellipse').attr('rx', rw).attr('ry', rw / 5).style('fill', 'none');
+        });
+        //gnodes.call(force.drag)
+
+        var glinks = svg.selectAll('.link').data(force.links());
+        glinks.enter().append('line').attr('class', function fn(link) {
+            return link.class;
+        });
+        glinks.exit().remove();
+    }
+
+    function collide(node, alpha) {
+        var flattening = 4;
+        var r = node.width + 16,
+            nx1 = node.x - r,
+            nx2 = node.x + r,
+            ny1 = node.y - r / flattening,
+            ny2 = node.y + r / flattening;
+        return function(quad, x1, y1, x2, y2) {
+            if (quad.point && (quad.point !== node)) {
+                var x = node.x - quad.point.x,
+                    y = node.y - quad.point.y,
+                    // 此处对 y 进行加倍，逻辑是：对于指定的 x，本来在 y=sqrt(r*r-x*x) 处满足条件 l < r 时
+                    // **就**触发碰撞，可是现在 y 在本身的 1/flattening 处才要触发，所以。。。
+                    l = Math.sqrt(x * x + y * y * flattening * flattening),
+                    r = node.width + quad.point.width;
+                if (l < r) {
+                    l = (l - r) / l * 0.5 * alpha;
+                    x *= l;
+                    y *= l;
+                    node.x -= x;
+                    node.y -= y;
+                    quad.point.x += x;
+                    quad.point.y += y;
+                }
+            }
+            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        };
     }
 });
